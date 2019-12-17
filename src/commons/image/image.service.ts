96a92783f4d9c5a7508ebd4f6876f24052330a62
Arturo Bernal <arturober@gmail.com>
2019-12-17 11:32:37 +0100

    paths in Windows for images

--- src/commons/image/image.service.ts
+++ src/commons/image/image.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
+import * as path from 'path';
 import * as download from 'image-downloader';
 
 @Injectable()
@@ -7,21 +8,21 @@ export class ImageService {
     saveImage(dir: string, photo: string): Promise<string> {
         const data = photo.split(',')[1] || photo;
         return new Promise((resolve, reject) => {
-          const filePath = `img/${dir}/${Date.now()}.jpg`;
+          const filePath = path.join('img', dir, `${Date.now()}.jpg`);
           fs.writeFile(filePath, data, {encoding: 'base64'}, (err) => {
             if (err) { reject(err); }
-            resolve(filePath);
+            resolve(`img/${dir}/${Date.now()}.jpg`);
           });
         });
     }
 
     async downloadImage(dir: string, url: string): Promise<string> {
-      const filePath = `img/${dir}/${Date.now()}.jpg`;
+      const filePath = path.join('img', dir, `${Date.now()}.jpg`);
       await download.image({
         url,
         dest: filePath,
       });
-      return filePath;
+      return `img/${dir}/${Date.now()}.jpg`;
     }
 
     removeImage(path: string) {
